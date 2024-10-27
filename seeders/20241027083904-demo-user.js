4f51ebda008d9495fdbe1969090fbbc82925f910
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-10-27 22:54:37 +0900

    더미 데이터 추가

--- seeders/20241027083904-demo-user.js
+++ seeders/20241027083904-demo-user.js
@@ -5,6 +5,18 @@ module.exports = {
     async up(queryInterface, Sequelize) {
         return queryInterface.bulkInsert("User", [
             {
+                user_id: 0x0,
+                username: "test0",
+                email: "test0@test.com",
+                email_verified: new Date(),
+            },
+            {
+                user_id: 0x1,
+                username: "test1",
+                email: "test1@test.com",
+                email_verified: new Date(),
+            },
+            {
                 username: "",
                 email: "kang@gmail.com",
                 created_at: new Date(),
