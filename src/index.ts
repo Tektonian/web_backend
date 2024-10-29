1f7fc59c76ac36c9be5b0711f5021b2415963a34
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-10-29 17:27:43 +0900

    채팅 서버 웹소켓 통합 작업 시작
    웹소켓 생성 / 연결 / 연결 해재 /  전송 코드 완성
    
    데이터베이스 쓰기 작업 확인
    
    코드 및 타입 작업 해야하는 부분 다수 존재

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import requestInfoRoutes from "./routes/requestInfoRoutes";
-import requestInfoSequelize from "./dbconfig/requestInfoDatabase";
-import { chatTest } from "./dbconfig/chatDatabase";
+import requestInfoRoutes from "./routes/requestInfoRoutes.js";
+//import requestInfoSequelize from "./dbconfig/requestInfoDatabase";
+import { chatTest } from "./dbconfig/chatDatabase.js";
+import { authConfig } from "./config/auth.config.js";
+import { ExpressAuth } from "@auth/express";
+import {
+    currentSession,
+    authenticateUser,
+} from "./middleware/auth.middleware.js";
+import { models } from "./models/index.js";
+import * as chatControl from "./controllers/chat/chat.js";
+import initChat from "./routes/chatRouter.js";
+import { createServer } from "http";
+
 const app = express();
 const PORT = process.env.PORT || 8080;
+process.env.NODE_ENV = "production";
+app.set("port", 3000);
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/request-info", requestInfoRoutes);
 
-chatTest();
+app.use("/api/auth/*", ExpressAuth(authConfig));
 
+/*
 requestInfoSequelize
     .sync()
     .then(() => {
@@ -26,3 +40,71 @@ requestInfoSequelize
     .catch((error) => {
         console.error("Failed to sync database:", error);
     });
+*/
+app.use(currentSession);
+
+// Routes
+app.get("/protected", async ({ req, res, next }: netProps) => {
+    res.json({ session: res.session });
+});
+
+app.get(
+    "/api/protected",
+    authenticateUser,
+    async ({ req, res, next }: netProps) => {
+        res.json(res.session);
+    },
+);
+
+app.get("/", async ({ req, res, next }: netProps) => {
+    res.json({
+        title: "Express Auth Example",
+        user: res.session?.user,
+    });
+});
+
+app.post("/chatRooms", async (req, res, next) => {
+    const sessionUser = res.session.user;
+    if (sessionUser === undefined) res.json("No session");
+    const dbUserData = await models.User.findOne({
+        where: { email: sessionUser.email },
+        attributes: ["user_id", "username", "email"],
+    });
+    const chatRooms = await chatControl.getChatRoomsByUser(
+        dbUserData?.dataValues,
+    );
+    res.json(chatRooms);
+});
+
+app.post("/chatContents", async (req, res, next) => {
+    const { chatroom_id } = req.body;
+    const sessionUser = res.session.user;
+    if (sessionUser === undefined) res.json("No session");
+    const dbUserData = await models.User.findOne({
+        where: { email: sessionUser.email },
+        attributes: ["user_id", "username", "email"],
+    });
+    const messages = await chatControl.getChatRoomMessagesBiz(
+        chatroom_id,
+        dbUserData?.dataValues,
+    );
+    console.log(messages);
+    res.json(messages);
+});
+
+chatTest();
+/*
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        credentials: true,
+        origin: "http://localhost:3000",
+    },
+});
+*/
+
+const httpServer = createServer(app);
+
+const io = initChat(httpServer);
+httpServer.listen(8080);
+//app.listen(8080, () => console.log("App listening"));
