59eaa2d42d4e7baae225e728df2355d5b6b38fa6
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-11 13:08:55 +0900

    리팩토링 완료
    1. 함수 스코프 별도로 분리해서 전역 변수로 사용할 수 있게 bind 시킴
    
    # 문제점
    1. JS 언어 한계상 스코프 또는 클로저 내에서 사용할 수 있는 변수에 재할당 시 변수 이름에 맞춰서 전달하는 것이 불가능 해서, 변수를 별도로 주입 해야함.
    
    추후에 어떤 코드가 좋을지는 조금 더 살펴봐야함.

--- src/routes/chat/webSocketRouter.ts
+++ src/routes/chat/webSocketRouter.ts
@@ -13,7 +13,9 @@ import type {
     IChatroom,
 } from "../../types/chat/chatSchema.types";
 import { ISessionUser } from "../../config/auth.types";
-
+interface resM {
+    recv: string;
+}
 const ResMessageRactory = (
     message: IChatContent,
     sender: IChatUser,
@@ -54,6 +56,24 @@ const ResMessagesFactory = (
     }
     // console.log("RET: ", messages);
     return ret;
+};
+
+const registerHelper = (eventFlow) => {
+    // list ([a]) to object {a: 'a'}
+    const globalArgs = eventFlow.globalArgs;
+    console.log("Global args", globalArgs, eventFlow.globalArgs);
+    const eventTargets = eventFlow.eventTargets;
+    const chains = eventFlow.chains;
+    for (let chain of chains) {
+        const target = eventTargets[chain.eventTarget];
+        const eventName = chain.eventName;
+        const localArgs = chain.args;
+        if (localArgs === undefined) {
+            target.on(eventName, chain.handler.bind(target, globalArgs));
+        } else {
+            target.on(eventName, chain.handler.bind(target, globalArgs));
+        }
+    }
 };
 
 export default function initChat(httpServer) {
@@ -110,188 +130,246 @@ export default function initChat(httpServer) {
             console.log("User failed to connect", e);
             socket.disconnect(true);
         }
-        console.log("chatUser.id ", chatUser._id.toString());
-        updateChatRoomEvent.on(
-            chatUser._id.toString(),
-            async ({ jobId, returnvalue }) => {
-                console.log("On update chat room", JSON.parse(returnvalue));
-                socket.emit("updateChatRoom", JSON.parse(returnvalue));
+        socket.data.chatUser = chatUser;
+
+        const eventRegisterFlow = {
+            globalArgs: {
+                io,
+                socket,
+                userSentEvent,
+                updateChatRoomEvent,
+                chatUserController,
+                chatRoomController,
+                chatUnreadController,
+                chatContentController,
+                chatUser,
             },
-        );
-
-        socket.on("userTryJoin", async function (req: reqTryJoinProps) {
-            // Is he ok to join?
-            const { chatRoomId, deviceLastSeq } = req;
-            const chatRoom: IChatroom | null =
-                await chatRoomController.getChatRoomById(chatRoomId);
-            console.log("messages", req);
-
-            // set socket.data if user joined a room
-            socket.data.chatRoom = chatRoom;
-            socket.data.user = chatUser;
-            console.log("Socket chatroom", socket.data.chatRoom);
-            if (chatRoom === null) {
-                return;
-                throw new Error(`Room not exist: ${chatRoomId}`);
-            }
-            if (!chatRoom.participant_ids.includes(chatUser.user_id)) {
-                throw new Error("User have no perssion to access a room");
-            }
-
-            // received unread messages and update unread schema
-            const currChatRoomSeq = chatRoom.message_seq;
-            const messages = [] as IChatContent[];
-            console.log("SEqs: ", currChatRoomSeq, deviceLastSeq);
-            if (currChatRoomSeq - deviceLastSeq > 0) {
-                // get unread messages
-                const unreadMessages =
-                    await chatContentController.getChatRoomMessagesBySeq(
-                        chatRoom,
-                        deviceLastSeq,
-                    );
-                messages.push(...unreadMessages);
-                // update unread schema
-                await chatUnreadController.updateUserUnread(
-                    chatUser,
-                    chatRoom,
-                    currChatRoomSeq,
-                );
-                // console.log("Unread, ", unreadMessages);
-            }
-            // get last read sequences
-            const lastReadSeqences =
-                await chatUnreadController.getUnreadSequences(chatRoom._id);
-
-            const resMessages = ResMessagesFactory(
-                messages,
-                chatUser,
-                lastReadSeqences as number[],
-            );
-
-            const res = JSON.stringify({
-                messages: resMessages,
-                lastReadSequences: lastReadSeqences,
-            });
-
-            const jobName = `${chatRoom._id.toString()}:${chatUser._id.toString()}`;
-            console.log("jobName: ", jobName);
-
-            userSentEvent.on(
-                jobName,
-                async ({
-                    jobId,
-                    returnvalue,
-                }: {
-                    jobId: string;
-                    returnvalue: string; // JSON.stringfied IChatContent
-                }) => {
-                    const participant_ids = chatRoom.participant_ids;
-                    // 2. emit user "otherSent"
-                    // and server waits for users' "updateLastRead"
-                    console.log("User Sent", jobId, returnvalue);
-                    const responses: resSomeoneSent[] = await io
-                        .in(chatRoom._id.toString())
-                        .timeout(500)
-                        .emitWithAck(
-                            "someoneSent",
-                            JSON.stringify(
-                                ResMessageRactory(
-                                    JSON.parse(returnvalue),
-                                    chatUser,
-                                    [0],
-                                ),
-                            ),
-                        );
-                    const respondUserIds = responses.map((res) => res.id);
-                    chatUnreadController.whetherSendAlarm(
-                        chatRoom,
-                        JSON.parse(returnvalue),
-                        participant_ids,
-                        respondUserIds,
-                    );
-                    // 3. user response that I have read a message so update last read
-                    // If no response then don't update last read
-                    console.log(
-                        "SomeoneSent responses: ",
-                        socket.id,
-                        responses,
-                    );
-                    console.log(
-                        "SomeoneSent returnvale: ",
-                        jobId,
-                        " ",
-                        returnvalue,
-                    );
+            eventTargets: {
+                socket,
+                userSentEvent,
+                updateChatRoomEvent,
+            },
+            chains: [
+                {
+                    eventTarget: "socket",
+                    eventName: "userTryJoin",
+                    handler: userTryJoinHandler,
                 },
-            );
-            try {
-                const response = await socket
-                    .timeout(500)
-                    .emitWithAck("userJoined", res);
-                // join user after acknowledgement
-                console.log("User joined: ", chatRoomId, " status: ", response);
-                socket.join(chatRoomId);
-            } catch (e) {
-                // should now reach here
-                throw new Error("User couldn't join the room");
-            }
-        });
+                {
+                    eventTarget: "socket",
+                    eventName: "userTryUnjoin",
+                    handler: userTryUnJoinHandler,
+                },
+                {
+                    eventTarget: "socket",
+                    eventName: "updateChatRoom",
+                    handler: updateChatRoomHandler,
+                },
+                {
+                    eventTarget: "socket",
+                    eventName: "updateLastRead",
+                    handler: updateLastReadHandler,
+                },
+                {
+                    eventTarget: "socket",
+                    eventName: "sendMessage",
+                    handler: sendMessageHandler,
+                },
+                {
+                    eventTarget: "socket",
+                    eventName: "disconnect",
+                    handler: socketDisconnectHandler,
+                },
+                {
+                    eventTarget: "userSentEvent",
+                    eventName: chatUser._id.toString(),
+                    handler: userSentEventHandler,
+                },
+                {
+                    eventTarget: "updateChatRoomEvent",
+                    eventName: chatUser._id.toString(),
+                    handler: updateChatRoomHandler,
+                },
+            ],
+        };
 
-        socket.on("sendMessage", async (recv, callback) => {
-            const req = JSON.parse(recv);
-            const chatRoom = socket.data.chatRoom;
-            console.log("message: ", recv);
-            console.log("chatRoom: ", chatRoom);
-
-            await chatContentController.sendMessage(
-                chatRoom._id,
-                chatUser,
-                req.message.content,
-            );
-        });
-
-        socket.on("updateLastRead", async (recv, callback) => {
-            const { lastSeq } = recv;
-            const chatRoom = socket.data.chatRoom;
-            chatUnreadController.updateUserUnread(
-                chatUser._id,
-                chatRoom,
-                lastSeq,
-            );
-        });
-
-        // 4. server send last read sequences of users
-        //socket.in(chatRoomId).emit("unreadSeq");
-
-        // when user leavs chatroom
-        socket.on("userTryUnJoin", () => {
-            const chatRoom = socket.data.chatRoom;
-            const chatUser = socket.data.chatUser;
-            if (chatRoom !== null) {
-                console.log("User leave room: ", chatRoom);
-                socket.leave(chatRoom._id);
-                const jobName = `${chatRoom._id.toString()}:${chatUser._id.toString()}`;
-                userSentEvent.removeAllListeners(jobName);
-            }
-            socket.data.chatRoom = null;
-        });
-
-        socket.on("disconnecting", () => {
-            console.log("User: ", chatUser, " leaves ", socket.rooms);
-        });
-
-        // when user leaves chatpage
-        socket.on("disconnect", async () => {
-            userSentEvent.disconnect();
-            updateChatRoomEvent.removeAllListeners(chatUser._id.toString());
-            updateChatRoomEvent.disconnect();
-            await chatUserController.delUserById(chatUser._id);
-            console.log("User disconnected: ", socket.data.chatRoom);
-            socket.data.chatRoom = null;
-            socket.data.user = null;
-            socket.disconnect(true);
-        });
+        registerHelper(eventRegisterFlow);
     });
 
     return io;
+}
+
+async function updateChatRoomHandler(globalArgs, { jobId, returnvalue }) {
+    const { socket } = globalArgs;
+    console.log("On update chat room", JSON.parse(returnvalue));
+    socket.emit("updateChatRoom", JSON.parse(returnvalue));
+}
+
+async function sendMessageHandler(globalArgs, recv) {
+    console.log("message: ", recv);
+    const { socket, chatContentController } = globalArgs;
+    const req = JSON.parse(recv);
+    const chatRoom = socket.data.chatRoom;
+    const chatUser = socket.data.chatUser;
+    console.log("chatRoom: ", chatRoom, chatUser);
+
+    await chatContentController.sendMessage(
+        chatRoom._id,
+        chatUser,
+        req.message.content,
+    );
+}
+
+async function updateLastReadHandler(globalArgs, recv) {
+    const { socket, chatUnreadController } = globalArgs;
+    const { lastSeq } = recv;
+    const chatUser = socket.data.chatUser;
+    const chatRoom = socket.data.chatRoom;
+    chatUnreadController.updateUserUnread(chatUser._id, chatRoom, lastSeq);
+}
+
+async function userTryUnJoinHandler(globalArgs) {
+    const { socket, userSentEvent } = globalArgs;
+    // 4. server send last read sequences of users
+    //socket.in(chatRoomId).emit("unreadSeq");
+
+    // when user leavs chatroom
+    const chatRoom = socket.data.chatRoom;
+    const chatUser = socket.data.chatUser;
+    if (chatRoom !== null) {
+        console.log("User leave room: ", chatRoom);
+        socket.leave(chatRoom._id);
+        const jobName = `${chatRoom._id.toString()}:${chatUser._id.toString()}`;
+        userSentEvent.removeAllListeners(jobName);
+    }
+    socket.data.chatRoom = null;
+}
+
+async function socketDisconnectHandler(globalArgs) {
+    const { socket, userSentEvent, updateChatRoomEvent, chatUserController } =
+        globalArgs;
+    const chatUser = socket.data.chatUser;
+    userSentEvent.disconnect();
+    updateChatRoomEvent.removeAllListeners(chatUser._id.toString());
+    updateChatRoomEvent.disconnect();
+    await chatUserController.delUserById(chatUser._id);
+    console.log("User disconnected: ", socket.data.chatRoom);
+    socket.data.chatRoom = null;
+    socket.data.user = null;
+    socket.disconnect(true);
+}
+
+async function userSentEventHandler(
+    globalArgs,
+    {
+        jobId,
+        returnvalue, // JSON.stringfied IChatContent
+    },
+) {
+    const { io, socket, chatUnreadController } = globalArgs;
+    const chatRoom = socket.data.chatRoom;
+    const chatUser = socket.data.chatUser;
+    const participant_ids = chatRoom.participant_ids;
+    // 2. emit user "otherSent"
+    // and server waits for users' "updateLastRead"
+    console.log("User Sent", jobId, returnvalue);
+    const responses: resSomeoneSent[] = await io
+        .in(chatRoom._id.toString())
+        .timeout(500)
+        .emitWithAck(
+            "someoneSent",
+            JSON.stringify(
+                ResMessageRactory(JSON.parse(returnvalue), chatUser, [0]),
+            ),
+        );
+    const respondUserIds = responses.map((res) => res.id);
+    chatUnreadController.whetherSendAlarm(
+        chatRoom,
+        JSON.parse(returnvalue),
+        participant_ids,
+        respondUserIds,
+    );
+    // 3. user response that I have read a message so update last read
+    // If no response then don't update last read
+    console.log("SomeoneSent responses: ", socket.id, responses);
+    console.log("SomeoneSent returnvale: ", jobId, " ", returnvalue);
+}
+
+async function userTryJoinHandler(globalArgs, req: reqTryJoinProps) {
+    console.log("argument", globalArgs);
+    const {
+        socket,
+        chatRoomController,
+        chatUser,
+        chatContentController,
+        chatUnreadController,
+    } = globalArgs;
+    // Is he ok to join?
+    const { chatRoomId, deviceLastSeq } = req;
+    const chatRoom: IChatroom | null =
+        await chatRoomController.getChatRoomById(chatRoomId);
+    console.log("messages", req);
+
+    // set socket.data if user joined a room
+    socket.data.chatRoom = chatRoom;
+    socket.data.user = chatUser;
+    console.log("Socket chatroom", socket.data.chatRoom);
+    if (chatRoom === null) {
+        return;
+        throw new Error(`Room not exist: ${chatRoomId}`);
+    }
+    if (!chatRoom.participant_ids.includes(chatUser.user_id)) {
+        throw new Error("User have no perssion to access a room");
+    }
+
+    // received unread messages and update unread schema
+    const currChatRoomSeq = chatRoom.message_seq;
+    const messages = [] as IChatContent[];
+    console.log("SEqs: ", currChatRoomSeq, deviceLastSeq);
+    if (currChatRoomSeq - deviceLastSeq > 0) {
+        // get unread messages
+        const unreadMessages =
+            await chatContentController.getChatRoomMessagesBySeq(
+                chatRoom,
+                deviceLastSeq,
+            );
+        messages.push(...unreadMessages);
+        // update unread schema
+        await chatUnreadController.updateUserUnread(
+            chatUser,
+            chatRoom,
+            currChatRoomSeq,
+        );
+        // console.log("Unread, ", unreadMessages);
+    }
+    // get last read sequences
+    const lastReadSeqences = await chatUnreadController.getUnreadSequences(
+        chatRoom._id,
+    );
+
+    const resMessages = ResMessagesFactory(
+        messages,
+        chatUser,
+        lastReadSeqences as number[],
+    );
+
+    const res = JSON.stringify({
+        messages: resMessages,
+        lastReadSequences: lastReadSeqences,
+    });
+
+    const jobName = `${chatRoom._id.toString()}:${chatUser._id.toString()}`;
+    console.log("jobName: ", jobName);
+
+    try {
+        const response = await socket
+            .timeout(500)
+            .emitWithAck("userJoined", res);
+        // join user after acknowledgement
+        console.log("User joined: ", chatRoomId, " status: ", response);
+        socket.join(chatRoomId);
+    } catch (e) {
+        // should now reach here
+        throw new Error("User couldn't join the room");
+    }
 }
