6266d5ec1557c32c796a747003a5c5965fddfb12
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-12 15:45:39 +0900

    SSE 테스트 코드 작성 및 테스트 완료

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { QueueEvents } from "bullmq";
+import { createSession } from "better-sse";
 import requestInfoRoutes from "./routes/requestInfoRoutes.js";
-//import requestInfoSequelize from "./dbconfig/requestInfoDatabase";
+import verificationRouter from "./routes/verificationRoute.js";
 import { chatTest } from "./dbconfig/chatDatabase.js";
 import { authConfig } from "./config/auth.config.js";
 import { ExpressAuth } from "@auth/express";
@@ -11,9 +13,13 @@ import {
     authenticateUser,
 } from "./middleware/auth.middleware.js";
 import { models } from "./models/index.js";
-import initChat from "./routes/chatRouter.js";
+import initChat from "./routes/chat/webSocketRouter.js";
 import { createServer } from "http";
 import { chatController } from "./controllers/chat/index.js";
+import { ResChatRoom } from "./types/chat/chatRes.types.js";
+import { IChatroom } from "./types/chat/chatSchema.types.js";
+
+const sseEvent = new QueueEvents("sendAlarm");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -48,6 +54,8 @@ app.get("/protected", async ({ req, res, next }: netProps) => {
     res.json({ session: res.session });
 });
 
+app.use("/verification", verificationRouter);
+
 app.get(
     "/api/protected",
     authenticateUser,
@@ -74,14 +82,37 @@ app.post("/chatRooms", async (req, res, next) => {
         await chatController.chatRoomController.getAllChatRoomsByUser(
             dbUserData?.dataValues,
         );
-    res.json(chatRooms);
+
+    const ResChatRoomFactory = async (chatRoom: IChatroom): ResChatRoom => {
+        const consumer = await chatController.chatUserController.getUserByUUID(
+            chatRoom.consumer_id,
+        );
+        const participants =
+            await chatController.chatUserController.getUsersByUUID(
+                chatRoom.participant_ids,
+            );
+        const consumerName = consumer?.username;
+        const participantNames = participants.map((part) => part.username);
+        const resChatroom: ResChatRoom = {
+            chatRoomId: chatRoom._id.toString(),
+            messageSeq: chatRoom.message_seq,
+            consumerName: consumerName,
+            providerNames: participantNames,
+        };
+
+        return resChatroom;
+    };
+
+    const resChatRooms = await Promise.all(
+        chatRooms.map((chatRoom) => ResChatRoomFactory(chatRoom)),
+    );
+    res.json(resChatRooms);
 });
 
 app.post("/chatContents", async (req, res, next) => {
     const { chatroom_id } = req.body;
     const sessionUser = res.session.user;
     if (sessionUser === undefined) res.json("No session");
-    console.log("Session User", sessionUser);
     const dbUserData = await models.User.findOne({
         where: { email: sessionUser.email },
         attributes: ["user_id", "username", "email"],
@@ -92,6 +123,27 @@ app.post("/chatContents", async (req, res, next) => {
             dbUserData?.dataValues,
         );
     res.json(messages);
+});
+
+app.get("/sse", async (req, res) => {
+    const session = await createSession(req, res, {
+        headers: { "access-control-allow-credentials": "true" },
+    });
+
+    const user = res.session?.user;
+    if (user === undefined) return;
+
+    const callback = ({ jobId, returnvalue }) => {
+        console.log("sseEvent: ", session);
+        session.push(returnvalue, "message");
+    };
+    console.log("SSE connected: ", user.id.toString("hex"));
+    sseEvent.on(`${user.id.toString("hex")}`, callback);
+
+    session.on("disconnected", () => {
+        console.log("SSE disconnected");
+        sseEvent.off(`${user.id.toString("hex")}`, callback);
+    });
 });
 
 chatTest();
