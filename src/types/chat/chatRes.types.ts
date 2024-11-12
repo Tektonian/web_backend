5e3586604ec2a12df1801a5ef24927fec383cd89
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-12 15:47:35 +0900

    채팅 Req, Res 타입 지정

---
+++ src/types/chat/chatRes.types.ts
@@ -0,0 +1,9 @@
+export interface ResChatRoom {
+    chatRoomId: string;
+    consumerName: string;
+    providerNames: [string];
+    messageSeq: number;
+    lastSender: string;
+    lastMessage: string;
+    lastSentTime: Date;
+}
---
+++ src/types/chat/chatSchema.types.ts
@@ -0,0 +1,34 @@
+import { ObjectId, Types } from "mongoose";
+
+export interface IChatContent {
+    chatroom: Types.ObjectId;
+    seq: number;
+    content_type: "text" | "image" | "file" | "map";
+    content: string;
+    sender_id: Types.UUID;
+    image_url: string;
+}
+
+export interface IChatroom {
+    request_id: Number;
+    consumer_id: Types.UUID;
+    participant_ids: [Types.UUID];
+    message_seq: Number;
+}
+
+export interface IChatUser {
+    user_id: Types.UUID;
+    username: string;
+    nationality: string;
+    multilingual: [string];
+    user_name_glb: Map<string, string>;
+    email: String;
+    image_url: String;
+}
+export interface IUnread {
+    chatroom: Types.ObjectId;
+    user_id: Types.UUID;
+    send_alarm: boolean;
+    last_read_at: Date;
+    last_read_seq: Number;
+}
