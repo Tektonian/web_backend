677576e422e0829300f3e8ca16a74b59b4d8f898
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-12 15:55:06 +0900

    인증 관련 미들 웨어 추가
    JSON으로 문자열 변환된 user_Id를 버퍼로 변환하는 코드 추가
    
    TODO: @auth 라이브러리를 변형해서 사용한다고 변수 이름과 같은 것들이 이상하게 설정되어 있음 추후에 정규화 필요

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { getSession } from "@auth/express";
 import { authConfig } from "../config/auth.config.js";
 import type { NextFunction, Request, Response } from "express";
-
+import getToken from "@auth/core";
 export const authenticateUser = async (
     req: Request,
     res: Response,
@@ -9,7 +9,6 @@ export const authenticateUser = async (
 ) => {
     const session =
         res.locals.session ?? (await getSession(req, authConfig)) ?? undefined;
-
     res.locals.session = session;
 
     if (session) {
@@ -26,6 +25,17 @@ export const currentSession = async (
 ) => {
     const session = (await getSession(req, authConfig)) ?? undefined;
     res.session = session;
+
+    // encode JSON.stringfied Buffer to Buffer
+    // https://stackoverflow.com/questions/34557889/how-to-deserialize-a-nested-buffer-using-json-parse
+    console.log("currentSEssion", session);
+    if (
+        session !== undefined &&
+        session.user !== undefined &&
+        session.user.id !== undefined
+    ) {
+        res.session.user.id = Buffer.from(session.user?.id.data);
+    }
 
     return next();
 };
