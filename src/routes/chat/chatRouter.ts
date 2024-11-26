1ad67f99eed1678f0c29c729ebabd3626505d51b
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-11-26 20:39:04 +0900

    Create chatRouter.ts
    채팅 관련 기타 기능 chatRouter 로 분리
    
    1. 유저 전체 unread count 받는 기능
    2. 유저 참여중인 chatroom에 관한 정보 제공

---
+++ src/routes/chat/chatRouter.ts
@@ -0,0 +1,56 @@
+/**
+ * Router regarding chattings.
+ * such as, unread message count and information about current chatrooms
+ */
+import { Router } from "express";
+import { getUnreadCountOfUser } from "../../controllers/chat/chatUnreadController";
+const ChatRouter = Router();
+
+ChatRouter.post("/unread", async (req, res) => {
+    const sessionUser = res.session.user;
+    if (sessionUser === undefined) res.json("No session");
+
+    const ret = await getUnreadCountOfUser(sessionUser.user_id);
+
+    res.json({ unreadCount: ret });
+});
+
+ChatRouter.post("/chatRooms", async (req, res, next) => {
+    const sessionUser = res.session.user;
+    if (sessionUser === undefined) res.json("No session");
+    const dbUserData = await models.User.findOne({
+        where: { email: sessionUser.email },
+        attributes: ["user_id", "username", "email"],
+    });
+    const chatRooms =
+        await chatController.chatRoomController.getAllChatRoomsByUser(
+            dbUserData?.dataValues,
+        );
+
+    const ResChatRoomFactory = async (chatRoom: IChatroom): ResChatRoom => {
+        const consumer = await chatController.chatUserController.getUserByUUID(
+            chatRoom.consumer_id,
+        );
+        const participants =
+            await chatController.chatUserController.getUsersByUUID(
+                chatRoom.participant_ids,
+            );
+        const consumerName = consumer?.username;
+        const participantNames = participants.map((part) => part.username);
+        const resChatroom: ResChatRoom = {
+            chatRoomId: chatRoom._id.toString(),
+            messageSeq: chatRoom.message_seq,
+            consumerName: consumerName,
+            providerNames: participantNames,
+        };
+
+        return resChatroom;
+    };
+
+    const resChatRooms = await Promise.all(
+        chatRooms.map((chatRoom) => ResChatRoomFactory(chatRoom)),
+    );
+    res.json(resChatRooms);
+});
+
+export default ChatRouter;
