e7d6588282f9bf74a1bd2bee3e35d6ad1a7fa5a2
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-12-02 20:47:31 +0900

    Update StudentController.ts
    학생 프로필 등록 시 검새 엔진에 연동될 수 있도록 변경

--- src/controllers/wiip/StudentController.ts
+++ src/controllers/wiip/StudentController.ts
@@ -5,14 +5,19 @@ import { StudentReview } from "../../models/rdbms/StudentReview";
 import { fullstudentprofile } from "../../models/rdbms/fullstudentprofile";
 import { sequelize } from "../../models/rdbms";
 import { AcademicHistory } from "../../models/rdbms/AcademicHistory";
+import { School } from "../../models/rdbms/School";
 import { ExamHistory } from "../../models/rdbms/ExamHistory";
+import { DataTypes } from "sequelize";
 
 const client = new MeiliSearch({
     host: "http://127.0.0.1:7700",
     apiKey: "1zBmtAMDjgWPGLcTPAhEy-kRZv44BzxywQ1UHPkIYE0",
 });
-const Request = models.Request;
 
+const studentSearch = client.index("student");
+studentSearch.updateFilterableAttributes(["_geo"]);
+
+const Request = models.Request;
 const StudentWithCurrentSchool = models.studentwithcurrentschool;
 
 export const getRecommendedStudentByRequest = async (request_id: number) => {
@@ -68,66 +73,104 @@ export const getInstReviewOfStudentByStudentId = async (student_id: number) => {
 };
 
 // TODO: Add type later
-export const createStudentIdentity = async (data) => {
-    const transaction = await sequelize.transaction();
-
+export const createStudentIdentity = async (
+    uuid: typeof DataTypes.UUID,
+    data,
+) => {
     try {
-        const { userType, academicHistory, examHistory, ...student } = data;
+        const ret = await sequelize.transaction(async (t) => {
+            const { userType, academicHistory, examHistory, ...student } = data;
 
-        const createdStudent = await Student.create(
-            {
-                name_glb: student.name_glb,
-                nationality: student.nationality,
-                user_id: res.session.user.id,
-                age: student.age,
-                email_verified: student.email_verified,
-                phone_number: student.phone_number,
-                emergency_contact: student.emergency_contact,
-                gender: student.gender,
-                image: student.image,
-                has_car: student.has_car,
-                keyword_list: student.keyword_list,
-            },
-            { transaction },
-        );
-
-        const studentId = createdStudent.student_id;
-
-        for (const history of academicHistory) {
-            const isAttending = history.status === "In progress" ? 1 : 0;
-            await AcademicHistory.create(
+            const createdStudent = await Student.create(
                 {
-                    school_id: history.school_id,
-                    student_id: studentId,
-                    degree: history.degree,
-                    start_date: history.start_date,
-                    end_date: history.end_date,
-                    status: history.status,
-                    faculty: history.faculty,
-                    school_email: history.school_email,
-                    is_attending: isAttending,
+                    name_glb: student.name_glb,
+                    nationality: student.nationality,
+                    user_id: uuid,
+                    age: student.age,
+                    email_verified: student.email_verified,
+                    phone_number: student.phone_number,
+                    emergency_contact: student.emergency_contact,
+                    gender: student.gender,
+                    image: student.image,
+                    has_car: student.has_car,
+                    keyword_list: student.keyword_list,
                 },
-                { transaction },
+                { transaction: t },
             );
-        }
 
-        for (const exam of examHistory) {
-            await ExamHistory.create(
-                {
-                    student_id: studentId,
-                    exam_id: exam.exam_id,
-                    exam_result: exam.exam_result,
-                },
-                { transaction },
+            const studentId = createdStudent.student_id;
+
+            let searchDocument = {
+                id: studentId,
+                ...createdStudent.dataValues,
+            };
+
+            for (const history of academicHistory) {
+                const isAttending = history.status === "In progress" ? 1 : 0;
+                const acaHistory = await AcademicHistory.create(
+                    {
+                        school_id: history.school_id,
+                        student_id: studentId,
+                        degree: history.degree,
+                        start_date: history.start_date,
+                        end_date: history.end_date,
+                        status: history.status,
+                        faculty: history.faculty,
+                        school_email: history.school_email,
+                        is_attending: isAttending,
+                    },
+                    { transaction: t },
+                );
+                if (isAttending === 1) {
+                    const school = (
+                        await School.findByPk(history.school_id)
+                    )?.get({ plain: true });
+                    console.log(school);
+                    searchDocument = {
+                        ...searchDocument,
+                        _geo: {
+                            lat: school.coordinate.coordinates[0],
+                            lng: school.coordinate.coordinates[1],
+                        },
+                    };
+                }
+            }
+
+            searchDocument = {
+                ...searchDocument,
+                academicHistory: academicHistory,
+                examHistory: examHistory,
+            };
+
+            const searchRet = await studentSearch.addDocuments(
+                [searchDocument],
+                { primaryKey: "id" },
             );
-        }
 
-        await transaction.commit();
+            const searchTask = await client.waitForTask(searchRet.taskUid);
 
-        return createdStudent;
+            for (const exam of examHistory) {
+                await ExamHistory.create(
+                    {
+                        student_id: studentId,
+                        exam_id: exam.exam_id,
+                        exam_result: exam.exam_result,
+                    },
+                    { transaction: t },
+                );
+            }
+
+            if (searchTask.status !== "succeeded") {
+                throw new Error(
+                    "No record created " + JSON.stringify(searchTask),
+                );
+            }
+            return createdStudent;
+        });
+
+        return ret;
     } catch (error) {
-        await transaction.rollback();
-        console.error("Transaction failed:", error);
-        return null;
+        console.error("Create student profile failed:", error);
+        return undefined;
     }
 };
