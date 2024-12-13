d8b00c0117ae947f364f4be9d215024612698d67
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-12-13 20:41:06 +0900

    Websocket Router 코드 정리 -> 이제부터 리팩토링 시작
    1. 데이터 타입 (APIType) 추가
    2. 주석 추가

--- src/routes/chat/webSocketRouter.ts
+++ src/routes/chat/webSocketRouter.ts
@@ -1,39 +1,38 @@
 import { createHash } from "crypto";
 import { QueueEvents } from "bullmq";
-import { HydratedDocument } from "mongoose";
+import { HydratedDocument, InferRawDocType, InferSchemaType } from "mongoose";
 import { Server } from "socket.io";
 
 import { currentSession } from "../../middleware/auth.middleware";
+
 import { chatController } from "../../controllers/chat";
 import * as UserController from "../../controllers/UserController";
-import { ChatContent } from "../../models/chat";
 
-import logger from "../../utils/logger";
+import { ChatContent, ChatUser, Types as ChatTypes } from "../../models/chat";
+
 import { APIType } from "api_spec";
-
-import type { Socket } from "socket.io";
-// TODO: remove
-import type {
-    reqTryJoinProps,
-    resMessage,
-    resSomeoneSent,
-} from "./webSocketRouter.types";
 import { ISessionUser } from "../../config/auth.types";
 
+import logger from "../../utils/logger";
+
+type ReqSendMessage = APIType.WebSocketType.ReqSendMessage;
+type ResSomeoneSent = APIType.WebSocketType.ResSomeoneSent;
+
+type ReqUpdateLastRead = APIType.WebSocketType.ReqUpdateLastRead;
+
+type ResChatRoom = APIType.ChatRoomType.ResChatRoom;
+
+type UserSentEventReturn = APIType.WebSocketType.UserSentEventReturn;
+type UpdateChatRoomReturn = APIType.WebSocketType.UpdateChatRoomReturn;
+
 const ResChatRoomFactory = async (
-    chatRoom: HydratedDocument<IChatroom>,
+    chatRoom: ChatTypes.ChatRoomType,
 ): Promise<ResChatRoom> => {
-    logger.debug(`ResChatRoomFactory: ${chatRoom.toJSON()}`);
-
-    const chatRoomJSON = chatRoom.toJSON();
-
     const consumer = (
-        await UserController.getUserById(Buffer.from(chatRoomJSON.consumer_id))
+        await UserController.getUserById(Buffer.from(chatRoom.consumer_id))
     )?.get({ plain: true });
 
-    const usersUUIDs = chatRoomJSON.participant_ids.map((id) =>
-        Buffer.from(id),
-    );
+    const usersUUIDs = chatRoom.participant_ids.map((id) => Buffer.from(id));
     const participantsInst = await UserController.getUsersById(usersUUIDs);
 
     const participants = participantsInst.map((inst) =>
@@ -44,27 +43,32 @@ const ResChatRoomFactory = async (
             chatRoom._id,
         );
 
+    logger.debug(
+        `ResChatRoomFactory: ${JSON.stringify(chatRoom)} ${JSON.stringify(lastMessage)}`,
+    );
     const participantsRes = participants.map((part) => {
         return {
             // IMPORTANT
             // _id: "" <- user's chat id should not respond
-            username: part.username,
+            user_name: part.username ?? "",
             user_id: createHash("sha256")
                 .update(part.user_id.toString("hex"))
                 .digest("hex"),
             email: part.email,
-            image_url: part.image,
+            image_url: part.image ?? "",
         };
     });
     const consumerName = consumer?.username;
     const participantNames = new Set(participants.map((part) => part.username));
     const resChatroom: ResChatRoom = {
+        title: chatRoom.title,
         chatRoomId: chatRoom._id.toString(),
         messageSeq: chatRoom.message_seq,
-        consumerName: consumerName,
-        providerNames: Array.from(participantNames),
+        lastSenderId: createHash("sha256")
+            .update(lastMessage.sender_id.toString("hex"))
+            .digest("hex"),
+        consumer: consumer,
         participants: participantsRes,
-        lastSender: "",
         lastMessage: lastMessage?.content,
         lastSentTime: lastMessage?.created_at,
     };
@@ -125,17 +129,48 @@ const ResMessagesFactory = (
     return ret;
 };
 
-// See pushUpdateChatRoom function in messageQueue.ts file
+/**
+ * Handler function of updateChatRoom event
+ * pushed event is in pushUpdateChatRoom
+ * @see {@linkcode pushUpdateChatRoom}
+ *
+ * @param returnvalue
+ *  {"message":
+ *      {"chatroom":"675bc0dfbc1bef09e87622f6",
+ *      "seq":0,
+ *      "content_type":"text",
+ *      "content":"sdf",
+ *      "sender_id":{"type":"Buffer","data":[118,97,207,146,184,38,17,239,130,8,10,117,44,127,249,98]},
+ *      "url":"",
+ *      "_id":"675bc14ebc1bef09e87623c2",
+ *      "created_at":"2024-12-13T05:08:30.212Z",
+ *      "updated_at":"2024-12-13T05:08:30.212Z","__v":0},
+ *  "chatUser":{
+ *      "_id":"675bc132bc1bef09e87623ad",
+ *      "user_id":{"type":"Buffer","data":[118,97,203,130,184,38,17,239,130,8,10,117,44,127,249,98]},
+ *      "user_name":"test0",
+ *      "multilingual":[],
+ *      "user_name_glb":{"en":"test0"},
+ *      "email":"test0@test.com",
+ *      "image_url":"",
+ *      "created_at":"2024-12-13T05:08:02.556Z",
+ *      "updated_at":"2024-12-13T05:08:02.556Z",
+ *      "__v":0}
+ * }
+ */
 async function updateChatRoomHandler(globalArgs, { jobId, returnvalue }) {
+    console.log(returnvalue);
+    logger.debug(`updateChatRoomHandler: ${returnvalue}`);
     const { socket } = globalArgs;
-    const { message, chatUser } = JSON.parse(returnvalue);
-    const ret = {
+    const { message }: { message: APIType.WebSocketType.UserSentEventReturn } =
+        JSON.parse(returnvalue);
+
+    const ret: APIType.WebSocketType.ResUpdateChatRoom = {
         _id: message._id,
         seq: message.seq,
         chatRoomId: message.chatroom,
-        contentType: "text",
+        contentType: message.content_type,
         content: message.content,
-        senderName: chatUser.username ?? "",
         createdAt: new Date(),
         updatedAt: new Date(),
     };
@@ -143,7 +178,18 @@ async function updateChatRoomHandler(globalArgs, { jobId, returnvalue }) {
     socket.emit("updateChatRoom", JSON.stringify(ret));
 }
 
-async function sendMessageHandler(globalArgs, recv) {
+/**
+ * When someone send a message.
+ * Messages are handled as following sequence
+ * 1. Chat content controller start processing @see {@linkcode sendMessage}
+ * 2. Push to message Queue @see {@linkcode pushMessageQueue}
+ *  1. Depending on ContentType some messages will have different processing sequence
+ * 3. If message queue worker successed a work (@see {@link userSentWorker})
+ *  1. A message should be stored in Chat DB
+ *  2. A message must have its own _id(ObjectId) and sequence
+ * 4. Finally event handler (@see {@link userSentEventHandler}) will be called
+ */
+async function sendMessageHandler(globalArgs, recv: ReqSendMessage) {
     logger.debug(`Send message Handler: Received: ${JSON.stringify(recv)}`);
     const { socket, chatContentController } = globalArgs;
     const chatRoom = socket.data.chatRoom;
@@ -151,7 +197,10 @@ async function sendMessageHandler(globalArgs, recv) {
     logger.debug(
         `Send message Handler: chatRoom: ${chatRoom}, chatUser: ${chatUser}, Message: ${JSON.stringify(recv)}`,
     );
-
+    if (!chatRoom || !chatUser) {
+        socket.disconnect();
+        return;
+    }
     // Check sender's temporary id is same as chatUser id
     if (recv.senderId !== chatUser._id.toString()) {
         // TODO: add throw error
@@ -160,12 +209,12 @@ async function sendMessageHandler(globalArgs, recv) {
 
     await chatContentController.sendMessage(
         chatRoom._id,
-        chatUser,
-        recv.content,
+        chatUser._id,
+        recv.message,
     );
 }
 
-async function updateLastReadHandler(globalArgs, recv) {
+async function updateLastReadHandler(globalArgs, recv: ReqUpdateLastRead) {
     const { socket, chatUnreadController } = globalArgs;
     const { lastSeq } = recv;
     const chatUser = socket.data.chatUser;
@@ -208,6 +257,19 @@ async function socketDisconnectHandler(globalArgs, reason) {
     await chatUserController.delChatUserById(chatUser._id);
 }
 
+/**
+ * 
+ * @param returnvalue
+    {"chatroom":"675ba490c6a4c1d9d85ade16",
+    "seq":30,
+    "content_type":"text",
+    "content":"asdf",
+    "sender_id":{"type":"Buffer","data":[118,97,203,130,184,38,17,239,130,8,10,117,44,127,249,98]},
+    "url":"",
+    "_id":"675ba5abc6a4c1d9d85adee1",
+    "created_at":"2024-12-13T03:10:35.175Z",
+    "updated_at":"2024-12-13T03:10:35.175Z","__v":0}
+ */
 async function userSentEventHandler(
     globalArgs,
     {
@@ -221,22 +283,21 @@ async function userSentEventHandler(
     const participant_ids = chatRoom.participant_ids;
     // 2. emit user "otherSent"
     // and server waits for users' "updateLastRead"
-    logger.debug(`User Sent Event: Return Value: ${JSON.parse(returnvalue)}`);
     const returnObject = JSON.parse(returnvalue);
     const objectDocument = new ChatContent({
         ...returnObject,
         sender_id: Buffer.from(returnObject.sender_id.data),
     });
 
-    const othersRes: Promise<resSomeoneSent[]> = socket
-        .in(chatRoom._id.toString())
+    const othersRes: Promise<ResSomeoneSent[]> = socket
+        .to(chatRoom._id.toString())
         .timeout(500)
         .emitWithAck(
             "someoneSent",
             JSON.stringify(ResMessageFactory(objectDocument, "inbound")),
         );
 
-    const senderRes: Promise<resSomeoneSent> = socket.emitWithAck(
+    const senderRes: Promise<ResSomeoneSent> = socket.emitWithAck(
         "someoneSent",
         JSON.stringify(ResMessageFactory(objectDocument, "outgoing")),
     );
@@ -261,7 +322,9 @@ async function userSentEventHandler(
     io.in(chatRoom._id.toString()).emit("updateUnread", lastReadSequences);
     // 3. user response that I have read a message so update last read
     // If no response then don't update last read
-    logger.debug(`SomeoneSent: Response: ${responses}`);
+    logger.debug(
+        `SomeoneSent: Response: ${JSON.stringify(responses)} ${JSON.stringify(chatRoom)}`,
+    );
     logger.debug(`SomeoneSent: Return Value: ${returnvalue}`);
 
     await chatUnreadController.whetherSendAlarm(
@@ -272,7 +335,10 @@ async function userSentEventHandler(
     );
 }
 
-async function userTryJoinHandler(globalArgs, req: reqTryJoinProps) {
+async function userTryJoinHandler(
+    globalArgs,
+    req: APIType.WebSocketType.ReqTryJoin,
+) {
     const {
         io,
         socket,
@@ -282,26 +348,31 @@ async function userTryJoinHandler(globalArgs, req: reqTryJoinProps) {
         chatUnreadController,
     } = globalArgs;
     const { chatRoomId, deviceLastSeq, id } = req;
-    const chatRoom: HydratedDocument<IChatroom> | null =
+    const chatRoom: HydratedDocument<ChatTypes.ChatRoomType> | null =
         await chatRoomController.getChatRoomById(chatRoomId);
-    logger.debug(`User Try join: ${req}`);
+    logger.debug(`User Try join: ${JSON.stringify(req)}`);
 
     // set socket.data if user joined a room
     socket.data.chatRoom = chatRoom;
     socket.data.chatUser = chatUser;
 
     if (chatRoom === null) {
+        logger.error(`Room not exist: ${chatRoomId}`);
         return;
-        throw new Error(`Room not exist: ${chatRoomId}`);
     }
     // Is he ok to join?
-    if (!chatRoom.participant_ids.includes(chatUser.user_id)) {
+    const isParticipant = chatRoom.participant_ids.find((id) =>
+        id.equals(chatUser.user_id),
+    );
+    if (isParticipant === undefined) {
+        logger.error(`User have no perssion to access a room `);
         return;
-        throw new Error("User have no perssion to access a room");
     }
     // Check users temporary id
     if (id !== chatUser._id.toString()) {
         logger.error(`Wrong Chat Id: User: ${chatUser} Id: ${id}`);
+        console.log(JSON.stringify(req));
+        console.log(req);
         return;
     }
 
@@ -450,7 +521,7 @@ export default function initChat(httpServer) {
 
     const io = new Server(httpServer, {
         cors: {
-            origin: "http://localhost:3000",
+            origin: process.env.CORS_ORIGIN,
             credentials: true,
         },
         path: "/api/chat",
@@ -478,16 +549,11 @@ export default function initChat(httpServer) {
         // MEANS!!! that you should not use ObjectId of user
         // TODO: 기기별 1개로 재한 필요함
         // 지금은 채팅 페이지에서 유저가 요청하는데로 생성하고 있음
-        let chatUser: HydratedDocument<IChatUser> | null =
+        let chatUser: HydratedDocument<ChatTypes.ChatUserType> | null =
             await chatUserController.getChatUserByUUID(sessionUser.id);
 
         if (chatUser === null) {
-            chatUser = await chatUserController.createChatUser({
-                user_id: sessionUser.id,
-                email: sessionUser.email,
-                username: sessionUser.name,
-                image: "",
-            });
+            chatUser = await chatUserController.createChatUser(sessionUser.id);
         }
 
         if (chatUser === null) {
@@ -497,18 +563,17 @@ export default function initChat(httpServer) {
 
         // then send user ObjectId to a client, so we can identify user
         try {
-            const chatRooms = await chatRoomController.getAliveChatRoomsByUser({
-                user_id: chatUser.user_id,
-            });
+            const chatRooms = await chatRoomController.getAliveChatRoomsByUser(
+                chatUser.user_id,
+            );
 
             const resChatRooms = await Promise.all(
                 chatRooms.map(async (chatRoom) => ResChatRoomFactory(chatRoom)),
             );
-
             const is_connected = await socket
                 .timeout(500)
                 .emitWithAck("connected", {
-                    id: chatUser._id,
+                    id: chatUser._id.toString(),
                     chatRooms: resChatRooms,
                 });
             logger.info(
