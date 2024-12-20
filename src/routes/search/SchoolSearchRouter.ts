befd7ec2099ea4aba09801017280030bb82d942b
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2024-12-20 13:25:02 +0900

    Update SchoolSearchRouter.ts
    Joi schema 테스트 하는 코드 추가

--- src/routes/search/SchoolSearchRouter.ts
+++ src/routes/search/SchoolSearchRouter.ts
@@ -1,7 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import MeiliSearch from "meilisearch";
+import * as Joi from "@hapi/joi";
 import { APISpec } from "api_spec";
+import { SchoolSearchScheme } from "api_spec/joi";
+import { ValidateSchema } from "../../utils/validation.joi";
 import logger from "../../utils/logger";
+import "joi-extract-type";
+import { requestofcorporation } from "../../models/rdbms/requestofcorporation";
 const client = new MeiliSearch({
     host: "http://127.0.0.1:7700",
     apiKey: "1zBmtAMDjgWPGLcTPAhEy-kRZv44BzxywQ1UHPkIYE0",
@@ -10,8 +15,7 @@ const client = new MeiliSearch({
 const SchoolSearchRouter = express.Router();
 
 SchoolSearchRouter.get("/" satisfies keyof APISpec.SearchSchoolAPISpec, (async (req, res) => {
-    const { country_code } = req.query;
-
+    const { country_code } = ValidateSchema(SchoolSearchScheme.ReqSearchSchoolScheme, req.query);
     const index = client.index(`school-name-${country_code}`);
 
     try {
@@ -26,6 +30,6 @@ SchoolSearchRouter.get("/" satisfies keyof APISpec.SearchSchoolAPISpec, (async (
     }
 
     return;
-}) as APISpec.SearchSchoolAPISpec["/"]["get"]["__handler"]);
+}) as APISpec.SearchSchoolAPISpec["/"]["get"]["handler"]);
 
 export default SchoolSearchRouter;
