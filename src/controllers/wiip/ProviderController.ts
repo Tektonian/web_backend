d07b2c395d28db141c2b02fb6ba4e9d5e53473b9
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2025-01-03 20:15:54 +0900

    Student review 생성 라우터 수정

--- src/controllers/wiip/ProviderController.ts
+++ src/controllers/wiip/ProviderController.ts
@@ -1,6 +1,15 @@
+import { Op } from "sequelize";
 import { Provider } from "../../models/rdbms/Provider";
 
 export const getProvidersByRequest = async (requestId: number) => {
     const providers = await Provider.findAll({ where: { request_id: requestId } });
     return providers;
+};
+
+export const getProviderOfRequestByStudentId = async (requestId: number, studentId: number) => {
+    const provider = await Provider.findOne({
+        where: { [Op.and]: [{ request_id: requestId }, { student_id: studentId }] },
+    });
+
+    return provider;
 };
--- src/routes/wiip/StudentReviewRouter.ts
+++ src/routes/wiip/StudentReviewRouter.ts
@@ -1,78 +1,77 @@
 import express from "express";
 import { StudentReview } from "../../models/rdbms/StudentReview";
-import { Request as RequestModel } from "../../models/rdbms/Request";
 import { APISpec } from "api_spec";
-import { Consumer } from "../../models/rdbms/Consumer";
+import { RequestEnum } from "api_spec/enum";
 import logger from "../../utils/logger";
-import { User } from "../../models/rdbms/User";
 import * as Errors from "../../errors";
 import { filterSessionByRBAC } from "../../middleware/auth.middleware";
 import { getStudentReviewsByStudentId } from "../../controllers/wiip/StudentReviewController";
+import { StudentReviewSchema } from "api_spec/joi";
+import { ValidateSchema } from "../../utils/validation.joi";
 import { pick } from "es-toolkit";
+import { getUserByConsumerId } from "../../controllers/UserController";
+import { getProviderOfRequestByStudentId } from "../../controllers/wiip/ProviderController";
+import { getRequestByRequestId } from "../../controllers/wiip/RequestController";
+import { Op } from "sequelize";
 const StudentReviewRouter = express.Router();
 
-StudentReviewRouter.post("/" satisfies keyof APISpec.StudentReviewAPISpec, (async (req, res) => {
-    logger.info("Start-Create student review");
-    const { consumer_id, student_id, request_id, ...reviewData } = req.body;
-    const sessionUser = res.session?.user;
+StudentReviewRouter.post(
+    "/" satisfies keyof APISpec.StudentReviewAPISpec,
+    // Check login
+    filterSessionByRBAC(),
+    (async (req, res) => {
+        logger.info("Start-Create student review");
+        const { consumer_id, student_id, request_id, ...reviewData } = ValidateSchema(
+            StudentReviewSchema.ReqCreateStudentReveiwSchema,
+            req.body,
+        );
 
-    if (sessionUser === undefined) {
-        res.json("Login first");
-        return;
-    }
-    const user = (
-        await User.findOne({
-            where: { user_id: sessionUser.id },
-        })
-    )?.get({ plain: true });
+        const sessionUser = res.session!.user;
 
-    if (user === undefined) {
-        res.json("Db error");
-        return;
-    }
+        const consumerUser = (await getUserByConsumerId(consumer_id))?.get({ plain: true });
+        const studentProvider = (await getProviderOfRequestByStudentId(request_id, student_id))?.get({ plain: true });
 
-    const request = (
-        await RequestModel.findOne({
-            where: { request_id },
-            attributes: ["student_id"],
-        })
-    )?.get({ plain: true });
+        const request = (await getRequestByRequestId(request_id))?.get({ plain: true });
 
-    if (request === undefined) {
-        res.json("No such request or Db error");
-        return;
-    }
+        if (!consumerUser) {
+            throw new Errors.ServiceExceptionBase("User requested wrong consumer id");
+        } else if (!consumerUser.user_id.equals(sessionUser.id)) {
+            throw new Errors.ServiceExceptionBase("Session user is not consumer of a request");
+        }
+        if (!studentProvider) {
+            throw new Errors.ServiceExceptionBase("Student is not a provider of a request");
+        }
+        if (!request) {
+            throw new Errors.ServiceExceptionBase("User requested wrong request id");
+        } else if (request.request_status !== RequestEnum.REQUEST_STATUS_ENUM.FINISHED) {
+            throw new Errors.ServiceExceptionBase("Request status is not FINISHED");
+        }
 
-    const consumer = (
-        await Consumer.findOne({
-            where: { consumer_id: consumer_id },
-        })
-    )?.get({ plain: true });
-
-    if (consumer === undefined) {
-        res.json("No such consumer");
-        return;
-    } else if (!consumer.user_id.equals(sessionUser.id)) {
-        res.json("Wrong consumer");
-        return;
-    }
-
-    try {
-        const createdReview = await StudentReview.create({
-            consumer_id,
-            student_id,
-            request_id,
-            request_url: "",
-            ...reviewData,
+        const checkAlreadyExist = await StudentReview.count({
+            where: { [Op.and]: [{ request_id: request.request_id }, { student_id: studentProvider.student_id }] },
         });
-        res.json({ status: "ok" });
-        logger.info("End-Create student review");
-    } catch (error) {
-        logger.error(`Error creating student review: ${error}`);
-        res.json({ status: "failed" });
-    }
-    return;
-}) as APISpec.StudentReviewAPISpec["/"]["post"]["__handler"]);
+
+        if (checkAlreadyExist !== 0) {
+            throw new Errors.ServiceExceptionBase("Review of student already created");
+        }
+
+        try {
+            const createdReview = await StudentReview.create({
+                consumer_id,
+                student_id,
+                request_id,
+                request_url: "",
+                ...reviewData,
+            });
+            res.status(200).end();
+            logger.info("End-Create student review");
+        } catch (error) {
+            logger.error(`Error creating student review: ${error}`);
+            res.json({ status: "failed" });
+        }
+        return;
+    }) as APISpec.StudentReviewAPISpec["/"]["post"]["__handler"],
+);
 
 StudentReviewRouter.get(
     "/list/:student_id" satisfies keyof APISpec.StudentReviewAPISpec,
@@ -92,7 +91,7 @@ StudentReviewRouter.get(
 
         // If user is not corp nor orgn, filter corp and orgn reviews
         if (!userRoles.has("corp") && !userRoles.has("orgn")) {
-            studentReviews = studentReviews.filter((val) => val.corp_id !== undefined || val.orgn_id !== undefined);
+            studentReviews = studentReviews.filter((val) => val.corp_id === undefined && val.orgn_id === undefined);
         }
 
         const studentReviewCards = studentReviews.map((review) =>
