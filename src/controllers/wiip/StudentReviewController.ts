da7f39eb19d53730caceb6ccdd24613e3f55ff99
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2025-01-03 18:50:27 +0900

    Get student review 라우터 추가

--- src/controllers/wiip/StudentReviewController.ts
+++ src/controllers/wiip/StudentReviewController.ts
@@ -3,3 +3,7 @@ import { StudentReview } from "../../models/rdbms/StudentReview";
 export const getStudentReviewsByRequestId = async (requestId: number) => {
     return await StudentReview.findAll({ where: { request_id: requestId } });
 };
+
+export const getStudentReviewsByStudentId = async (studentId: number) => {
+    return await StudentReview.findAll({ where: { student_id: studentId } });
+};
--- src/routes/wiip/StudentReviewRouter.ts
+++ src/routes/wiip/StudentReviewRouter.ts
@@ -5,6 +5,10 @@ import { APISpec } from "api_spec";
 import { Consumer } from "../../models/rdbms/Consumer";
 import logger from "../../utils/logger";
 import { User } from "../../models/rdbms/User";
+import * as Errors from "../../errors";
+import { filterSessionByRBAC } from "../../middleware/auth.middleware";
+import { getStudentReviewsByStudentId } from "../../controllers/wiip/StudentReviewController";
+import { pick } from "es-toolkit";
 const StudentReviewRouter = express.Router();
 
 StudentReviewRouter.post("/" satisfies keyof APISpec.StudentReviewAPISpec, (async (req, res) => {
@@ -69,5 +73,49 @@ StudentReviewRouter.post("/" satisfies keyof APISpec.StudentReviewAPISpec, (asyn
     }
     return;
 }) as APISpec.StudentReviewAPISpec["/"]["post"]["__handler"]);
+
+StudentReviewRouter.get(
+    "/list/:student_id" satisfies keyof APISpec.StudentReviewAPISpec,
+    // need login
+    filterSessionByRBAC(),
+    (async (req, res) => {
+        logger.info("START-Get student review list");
+        const student_id = req.params.student_id;
+        const sessionUser = res.session!.user;
+        const userRoles = new Set(sessionUser.roles);
+
+        if (!student_id) {
+            throw new Errors.ServiceExceptionBase("User requested wrong student_id");
+        }
+
+        let studentReviews = (await getStudentReviewsByStudentId(student_id)).map((val) => val.get({ plain: true }));
+
+        // If user is not corp nor orgn, filter corp and orgn reviews
+        if (!userRoles.has("corp") && !userRoles.has("orgn")) {
+            studentReviews = studentReviews.filter((val) => val.corp_id !== undefined || val.orgn_id !== undefined);
+        }
+
+        const studentReviewCards = studentReviews.map((review) =>
+            pick(review, [
+                "consumer_id",
+                "student_id",
+                "request_id",
+                "was_late",
+                "was_diligent",
+                "was_proactive",
+                "commu_ability",
+                "lang_fluent",
+                "goal_fulfillment",
+                "want_cowork",
+                "praise",
+                "need_improve",
+                "need_improve",
+            ]),
+        );
+
+        res.status(200).json(studentReviewCards);
+        logger.info("END-Get student review list");
+    }) as APISpec.StudentReviewAPISpec["/list/:student_id"]["get"]["handler"],
+);
 
 export default StudentReviewRouter;
