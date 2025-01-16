26c04718c2a67a702c5ffec7cbc855922f8a0a73
gangjeuk <65859590+gangjeuk@users.noreply.github.com>
2025-01-16 14:10:34 +0900

    더미 데이터 생성 코드 수정. 불필요한 필드 sed 명령어로 replace 하도록

--- set_test_env.sh
+++ set_test_env.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 SEQ=sequelize
+DB_NAME=tektonian
+DB_URL=mysql://$2:$3@localhost/${DB_NAME}
+
+DB_USER=$2
+DB_PASSWORD=$3
 
 function dir {
     echo "[1] make directory"
@@ -18,8 +23,8 @@ function dir {
 function copy {
     echo "[3] copying datas"
 
-    cp seeders/* ${SEQ}/seeders/
-    npx sequelize-auto -h "localhost" -d "tektonian" -u "test_user" -e "mysql" -o "${SEQ}/models" -v true -p 3306
+    cp seeders/* ${SEQ}/seeders
+    npx sequelize-auto -h "localhost" -d ${DB_NAME} -u ${DB_USER} -x ${DB_PASSWORD} -e mysql -o "${SEQ}/models" -v true -p 3306
     echo "[4] copying done"
 }
 
@@ -29,6 +34,12 @@ function set {
 
     dir
     copy
+
+    echo "Replace strings, timestamps: true -> timestamps: false"
+    sed -i "" -e "s/timestamps: true/timestamps: false/g" ${SEQ}/models/*
+
+    echo "Replace of strings, type: "POINT" -> type: DataTypes.GEOMETRY"
+    sed -i "" -e 's/type: "POINT"/type: DataTypes.GEOMETRY/g' ${SEQ}/models/*
 }
 
 function clean {
@@ -40,7 +51,7 @@ function gen_data {
     echo "Check sequelize/config file before you run the script"
     cp seeders/* ${SEQ}/seeders/
     cd ${SEQ}
-    npx sequelize-cli db:seed --seed dummy-orgn dummy-user dummy-consumer dummy-school dummy-student dummy-request dummy-search dummy-review dummy-exam
+    npx sequelize-cli db:seed --url ${DB_URL} --seed dummy-orgn dummy-user dummy-consumer dummy-school dummy-student dummy-request dummy-search dummy-review dummy-exam
     cd ..
 }
 
@@ -48,12 +59,12 @@ function clean_data {
     echo "Cleaning data"
     echo "Check sequelize/config file before you run the script"
     cd ${SEQ}
-    npx sequelize-cli db:seed:undo:all
+    npx sequelize-cli --url ${DB_URL} db:seed:undo:all
     cd ..
 }
 
 function help {
-    echo "usage: bash set_test_env [function]"
+    echo "usage: bash set_test_env [function] [DB user name] [DB user password]"
     echo "function list"
     echo "set: Prepare test setting"
     echo "gen_data: generating dummy data"
@@ -61,7 +72,7 @@ function help {
     echo "clean: remove sequelizer folder"
 }
 
-if [[ $# -eq 0 ]] ;
+if [[ $# -ne 3 ]] ;
 then
     help
 else
